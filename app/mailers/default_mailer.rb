78ed3c30c78ebd7d14c767632fb87e61e65a6178
Tobias Mende <tobias.mende@tobsolution.de>
2012-10-22 17:06:18 +0200

    undo

--- app/mailers/default_mailer.rb
+++ app/mailers/default_mailer.rb
@@ -3,5 +3,5 @@ class DefaultMailer < ActionMailer::Base
   default from: "noreply@xn--epic-mrderspiel-etb.de"
   default content_type: "text/html"
   layout "email"
-  default from: quote_if_necessary(APP_CONFIG["default_name"], "utf-8") + " <" + APP_CONFIG["from_mail"]+">"
+  default from: APP_CONFIG["default_name"] + " <" + APP_CONFIG["from_mail"]+">"
 end
