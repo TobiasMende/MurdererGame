011023a88265d9958286131721d2aa717afb21f2
Tobias Mende <tobias.mende@tobsolution.de>
2012-10-22 17:27:23 +0200

    Mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ MurdererGame::Application.configure do
   config.assets.digest = true
   
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => "epic-mörderspiel.de" }
 
