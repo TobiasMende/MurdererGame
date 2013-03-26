37357d90eae5de1db4fb57e5212858aa7a69f4a8
Tobias Mende <tobias.mende@tobsolution.de>
2013-03-26 17:35:57 +0100

    Fixing kill cycle

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -7,50 +7,57 @@ class Contract < ActiveRecord::Base
   validates_uniqueness_of :murderer_id, :scope => [:game_id, :victim_id]
   validates_presence_of :game_id
   validates_presence_of :victim_id
-  validates_presence_of :murderer_id  
- 
- 
+  validates_presence_of :murderer_id
   def self.new_contract(game, murderer, victim)
-      c = Contract.new
-      c.murderer= murderer
-      c.victim= victim
-      c.game= game
-      c.save!
-      c
+    c = Contract.new
+    c.murderer= murderer
+    c.victim= victim
+    c.game= game
+    c.save!
+    c
   end
-  
+
   def confirm
-    new_contract = self.reconnect_chain
-    if !new_contract.nil?
-      ContractMailer.contract_accepted(self, new_contract).deliver
+    if victim.unproved_kill_contracts_in_game(self.game).empty?
+      # Find next murderer which is alive:
+      m = self.murderer
+      while !m.is_alive_in_game(game) && !(m == self.victim)
+        m = m.proved_victim_contracts_for_game(self.game).last.murderer
+      end
+    # reconnect chain m --> victim
+    new_contract = self.reconnect_chain(m)
+    ContractMailer.new_contract(new_contract).deliver
     end
-  end
-  
-  def reconnect_chain
-    # Contract where the current victim is the murderer
-    victims_contract = self.victim.current_kill_contracts.where("game_id = ?", self.game).first
-    
-    # New contract connecting current murderer with victims victim
-    
-    
-    # Delte old contract of the victim (is can't be executed)
     self.proved_at = Time.now
-      self.save
-      tmp = victims_contract
-      victims_contract.delete
+    self.save
+  # new_contract = self.reconnect_chain
+  # if !new_contract.nil?
+  # ContractMailer.contract_accepted(self, new_contract).deliver
+  # end
+  end
+
+  def reconnect_chain(new_murderer)
+    # Contract where the current victim is the murderer
+    victims_contract = self.victim.current_kill_contracts_for_game(self.game)
+
+    # New contract connecting current murderer with victims victim
+
+    # Delte old contract of the victim (is can't be executed)
+    tmp = victims_contract
+    victims_contract.delete
     # Handle game end condition
-    if tmp.victim == self.murderer
+    if tmp.victim == new_murderer
       self.game.handle_game_finished
       nil
     else
       new_contract = Contract.new
     new_contract.game = self.game
-    new_contract.murderer= self.murderer
+    new_contract.murderer= new_murderer
     new_contract.victim = tmp.victim
-      
-      new_contract.save
-      new_contract
+
+    new_contract.save
+    new_contract
     end
   end
-  
- end
+
+end
